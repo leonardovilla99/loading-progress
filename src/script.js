5a9aeb0a0336dea1d220a5d1595fc17d4dfe04bb
Leonardo Villa <info@leographic.it>
2022-01-29 20:31:03 -0500

    v 0.2

--- src/script.js
+++ src/script.js
@@ -2,12 +2,32 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import gsap from 'gsap'
 
 /**
  * Loaders
  */
-const gltfLoader = new GLTFLoader()
-const cubeTextureLoader = new THREE.CubeTextureLoader()
+const loadingBarElement = document.querySelector('.loading-bar')
+
+const loadingManager = new THREE.LoadingManager(
+
+    // Loaded
+    () =>{
+        window.setTimeout(() => {
+            gsap.to(overlayMaterial.uniforms.uAlpha, {duration: 3, value:0})
+            loadingBarElement.classList.add('ended')
+            loadingBarElement.style.transform = ''
+        }, 500)
+    },
+
+    // Progress
+    (itemURL, itemLoaded, itemsTotal) =>{
+        const progressRatio =  itemLoaded / itemsTotal
+        loadingBarElement.style.transform = 'scale(' +progressRatio + ')'
+    },
+)
+const gltfLoader = new GLTFLoader(loadingManager)
+const cubeTextureLoader = new THREE.CubeTextureLoader(loadingManager)
 
 /**
  * Base
@@ -20,6 +40,24 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * Overlay
+ */
+const overlayGeometry = new THREE.PlaneBufferGeometry(2,2,1,1)
+const overlayMaterial = new THREE.ShaderMaterial({
+    transparent: true,
+    uniforms:
+    {
+        uAlpha: {value: 1}
+    },
+    vertexShader: 'void main(){ gl_Position = vec4(position, 1.0);}',
+    fragmentShader: 'uniform float uAlpha; void main(){ gl_FragColor = vec4(0.0, 0.0, 0.0, uAlpha);}'
+})
+const overlay = new THREE.Mesh(overlayGeometry, overlayMaterial)
+
+scene.add(overlay)
+
 
 /**
  * Update all materials
